5cbc3e901db606f4c7c06946e960cd724f6e6715
Nagendra <vicky-nagendra-428@gmail.com>
2018-06-02 10:26:17 +0700

    Updated some naming conventions

--- src/main/java/com/assurity/common/ApiExecutor.java
+++ src/main/java/com/assurity/common/ApiExecutor.java
@@ -2,18 +2,22 @@ package com.assurity.common;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.hamcrest.core.IsNull;
 
 public class ApiExecutor {
 
     private Response response;
 
-    public Response loadTheEndPointGetMethod(String endPoint) {
+    public Response loadTheEndPoint_GetMethod(String endPoint) {
         response = RestAssured.get(endPoint);
         response.then().log().ifError();
         return response;
     }
 
     public Response getResponse() {
+        if (response == null) {
+
+        }
         return response;
     }
 
